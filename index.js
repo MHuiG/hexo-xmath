fa49a27766e0abc6f9592a3406c4ce8778567b2c
H <38281581+MHuiG@users.noreply.github.com>
2025-10-24 07:31:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
 'use strict';
 
 const config = require('./lib/config')(hexo);
-const {filter_md, filter_math,  render} = require('./lib/process');
+const {filter_md, filter_math,  render} = require('./lib/process')(hexo);
 
 hexo.extend.filter.register('before_post_render', data => {filter_md(config,data)}, 5);
 hexo.extend.filter.register('after_post_render', async(data) => {await filter_math(config,data)}, 5);
 hexo.extend.filter.register('after_render:html', (html, { page }) => {return render(config,html,page)});
+
