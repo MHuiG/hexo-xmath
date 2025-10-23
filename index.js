f5a74d76c7bffc5fa2c046a93d9d5c0189edde3a
H <38281581+MHuiG@users.noreply.github.com>
2025-10-24 06:31:19 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,32 +3,8 @@
 'use strict';
 
 const config = require('./lib/config')(hexo);
-const mathjax = require('./lib/mathjax')(config);
-const md = require('./lib/fuck-md')(config);
+const {filter_md, filter_math,  render} = require('./lib/process');
 
-
-hexo.extend.filter.register('before_post_render', data => {
-  if (!config.fuck_md) return;
-  if ((!data.mathjax && !data.math && !data.xmath) && !config.every_page) return;
-  data.content = md(data.content);
-  return data;
-}, 5);
-
-
-hexo.extend.filter.register('after_post_render', async(data) => {
-  if ((!data.mathjax && !data.math && !data.xmath) && !config.every_page) return;
-  data.content = await mathjax(data.content,data.tex_config || {});
-  return data;
-}, 5);
-
-
-if (config.append_css) {
-  const css = require('./lib/css')();
-  
-  hexo.extend.filter.register('after_render:html', (html, { page }) => {
-    if (config.every_page || (page.mathjax||page.math||page.xmath) || (page.__index && (page.posts.toArray().find(post => post.mathjax)||page.posts.toArray().find(post => post.math)||page.posts.toArray().find(post => post.xmath)))) {
-      html = html.replace('</head>', `<style>${css}</style></head>`);
-    }
-    return html;
-  });
-}
+hexo.extend.filter.register('before_post_render', data => {filter_md(config,data)}, 5);
+hexo.extend.filter.register('after_post_render', async(data) => {await filter_math(config,data)}, 5);
+hexo.extend.filter.register('after_render:html', (html, { page }) => {return render(config,html,page)});
